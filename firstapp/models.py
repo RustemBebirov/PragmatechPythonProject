40c3a1b5c3b0056b6752b62836891286ff003925
RustemBebirov <80380074+RustemBebirov@users.noreply.github.com>
2021-06-25 10:24:06 +0400

    Ctreated Blog models

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    """in this table blog information"""
+
+    Blog_categories = (
+        (1,'Travel'),
+        (2,'Food')
+    )
+
+    title = models.CharField('Bashliq',max_length=127,)
+    short_description = models.TextField('Qisa mezmun', blank=True )
+    description = models.TextField("Mezmun", null=True)
+    image = models.ImageField("Shekil", upload_to='blog_image')
+    category = models.IntegerField('Kategory', choices=Blog_categories )
--- firstapp/views.py
+++ firstapp/views.py
@@ -20,13 +20,42 @@ def shop(request):
 def shop_single(request):
     return render(request, 'shop-single.html')
 
+def events(request):
+    return render(request, 'events.html')
+
+def events_single(request):
+    return render(request, 'events-single.html')
+
+def teachers(request):
+    return render(request, 'teachers.html')
+
+def teachers_single(request):
+    return render(request, 'teachers-single.html')
+
+def courses(request):
+    return render(request, 'courses.html')
+
+def courses_single(request):
+    return render(request, 'courses-single.html')
+
 def gallery(request):
     return render(request, 'gallery.html')
 
 def contact(request):
     return render(request, 'contact.html')
 
+def about(request):
+    return render(request, 'about.html')
 
+def policy(request):
+    return render(request, 'policy.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+
+
+#homework 
 def homework(request):
     return render(request, 'homework.html')
 
