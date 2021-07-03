32a834b27286ce56f6eb237c2cd02f2e05290ccb
RustemBebirov <80380074+RustemBebirov@users.noreply.github.com>
2021-07-03 10:08:37 +0400

    install jet-admin

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp',
+    
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -64,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('jet/', include('jet.urls','jet')),
     path('', include('firstapp.urls', namespace='firstapp')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
