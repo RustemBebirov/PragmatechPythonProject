4d7778461bb2a4b6c35d82d2e2981953b31589c3
RustemBebirov <bebirov@bk.ru>
2021-07-28 13:47:29 +0400

    update

--- shop/views.py
+++ shop/views.py
@@ -20,54 +20,54 @@ class OrderDetailView(DetailView):
     template_name = 'shop-single.html'
     context_object_name = 'order'
 
-def like_order(request):
-    if request.method == 'POST':
-        liked_orders = request.COOKIES.get('liked_orders', '')
-        order_id = request.POST.get('order_id')
-    html = render_to_string('successfuly_liked.html')
-    response = HttpResponse(html)
-    if order_id not in liked_orders.split(','):
-        response.set_cookie("liked_orders",f'{liked_orders}{order_id},')
-    return response
+# def like_order(request):
+#     if request.method == 'POST':
+#         liked_orders = request.COOKIES.get('liked_orders', '')
+#         order_id = request.POST.get('order_id')
+#     html = render_to_string('successfuly_liked.html')
+#     response = HttpResponse(html)
+#     if order_id not in liked_orders.split(','):
+#         response.set_cookie("liked_orders",f'{liked_orders}{order_id},')
+#     return response
 
-def like_order_page(request):
-    like_orders = request.COOKIES.get('liked_orders','')
-    liked_order_ids=[]
-    for i in like_orders:
-        try:
-            j=int(i)
-            liked_order_ids.append(j)
-        except:
-            print('None')
+# def like_order_page(request):
+#     like_orders = request.COOKIES.get('liked_orders','')
+#     liked_order_ids=[]
+#     for i in like_orders:
+#         try:
+#             j=int(i)
+#             liked_order_ids.append(j)
+#         except:
+#             print('None')
 
-    orders = Order.objects.filter(id__in=liked_order_ids)
-    context ={
-        'orders': orders
-    }
-    return render(request,'liked_orders.html',context)
+#     orders = Order.objects.filter(id__in=liked_order_ids)
+#     context ={
+#         'orders': orders
+#     }
+#     return render(request,'liked_orders.html',context)
 
-def add_order(request):
-    if request.method == 'POST':
-        add_orders = request.COOKIES.get('add_orders', '')
-        order_id = request.POST.get('order_id')
-    html = render_to_string('successfuly_added.html')
-    response = HttpResponse(html)
-    if order_id not in add_orders.split(','):
-        response.set_cookie("add_orders",f'{add_orders}{order_id},')
-    return response
+# def add_order(request):
+#     if request.method == 'POST':
+#         add_orders = request.COOKIES.get('add_orders', '')
+#         order_id = request.POST.get('order_id')
+#     html = render_to_string('successfuly_added.html')
+#     response = HttpResponse(html)
+#     if order_id not in add_orders.split(','):
+#         response.set_cookie("add_orders",f'{add_orders}{order_id},')
+#     return response
 
-def add_order_page(request):
-    add_orders = request.COOKIES.get('add_orders','')
-    add_order_ids=[]
-    for i in add_orders:
-        try:
-            j=int(i)
-            add_order_ids.append(j)
-        except:
-            print('None')
+# def add_order_page(request):
+    # add_orders = request.COOKIES.get('add_orders','')
+    # add_order_ids=[]
+    # for i in add_orders:
+    #     try:
+    #         j=int(i)
+    #         add_order_ids.append(j)
+    #     except:
+    #         print('None')
 
-    orders = Order.objects.filter(id__in=add_order_ids)
-    context ={
-        'orders': orders
-    }
-    return render(request,'add_orders.html',context)
+    # orders = Order.objects.filter(id__in=add_order_ids)
+    # context ={
+    #     'orders': orders
+    # }
+    # return render(request,'add_orders.html',context)
